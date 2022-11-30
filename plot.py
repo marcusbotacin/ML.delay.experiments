23338ae1c0cff526b28e1ab57fb0bdffc324f255
Marcus Botacin <mfbotacin@Inf.ufpr.br>
2022-11-30 14:23:29 -0300

    more results

--- plot.py
+++ plot.py
@@ -4,8 +4,10 @@ import numpy as np
 import csv
 import sys
 
+# Large fonts, better to read in single column papers
 plt.rcParams['font.family'] = 'Roboto'
 plt.rcParams['font.weight'] = 600
+# had to decrease line size to differentiate points when close (is there a better solution?)
 plt.rcParams['lines.markersize'] = 1
 
 font = {
@@ -13,48 +15,33 @@ font = {
     'weight': 600
 }
 
-# Argv[1] = maximum exposure
-# Argv[2] = no balance
-# Argv[3] = balance 10:1
+# Trying to generate reproducible figs from the paper, so read from the same file
+files = ["data/no_detection_maximum_exposure.csv", "data/detection_no_drift_no_balance.exp2.csv", "data/detection_no_drift_balance_10.exp2.csv", "data/detection_drift_no_balance.exp2.csv", "data/detection_drift_balance_10.exp2.csv"]
 
-data = [x for x in csv.reader(open(sys.argv[1],'r'))][0]
-data2 = []
-for x in data:
-    try:
-        data2.append(float(x))
-    except:
-        pass
-dataX = [x for x in range(len(data2))]
+# We need to specify labels, in the correct order
+# Let's try to put labels in the exposure order, for easing the reading
+labels = ["No Detection", "No Drift-Imbalanced", "No Drift-10balanced", "Drift-Imbalanced", "Drift-10balanced"]
 
-data = [x for x in csv.reader(open(sys.argv[2],'r'))][0]
-data3 = []
-for x in data:
-    try:
-        data3.append(int(x))
-    except:
-        pass
-
-s_X = len(data2)
-s_Y = len(data3)
-
-if (s_X > s_Y):
-    data2 = data2[:s_Y]
-    dataX = [x for x in range(len(data3))]
-else:
-    data3 = data3[:s_X]
+def get_arrays(filename):
+    data = [x for x in csv.reader(open(filename,'r'))][0]
+    data2 = []
+    for x in data:
+        try:
+            # sometimes need to convert data types
+            # in most cases, we do not need this conversion, just return the first array
+            data2.append(int(x))
+        except:
+            pass
     dataX = [x for x in range(len(data2))]
+    return dataX, data2
 
-data = [x for x in csv.reader(open(sys.argv[3],'r'))][0]
-data4 = []
-for x in data:
-    try:
-        data4.append(int(x))
-    except:
-        pass
+vectors = []
+# open each filename and get the exposure rates for each one
+for i in files:
+    vectors.append(get_arrays(i))
 
-# fig = plt.figure(figsize =(9, 9))
+# Plot configuration, boring stuff
 fig, ax = plt.subplots(figsize =(10, 5), constrained_layout=True)
-
 #plt.xticks(rotation=90)
 plt.ylabel('Exposure (#)', fontdict=font)
 plt.ylim(0,750000)
@@ -64,9 +51,11 @@ plt.xticks(np.arange(0,260,10))
 plt.title('Malware Exposure over time', fontdict=font)
 plt.xlabel('Elapsed Epochs (#)', fontdict=font)
 
-plt.plot(dataX, data2, label="No Detection", marker='o', linestyle='--') #markersize=1)
-plt.plot(dataX, data3, label="No Balance-No Retrain", marker='o', linestyle='--') #markersize=1)
-plt.plot(dataX, data4, label="10: Balance-No Retrain", marker='s', linestyle='--') #markersize=1)
+# plot the points previously read from files
+for i, v in enumerate(vectors):
+    # we must ensure all Xs are equals
+    # Plot X,Y, and associated labels
+    plt.plot(v[0], v[1], label=labels[i], marker='o', linestyle='--') #markersize=1)
 
 plt.legend(loc="upper left")
 plt.grid(axis='both')
